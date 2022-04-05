1b1bae3af7dac47d509ab42d12b2194755663ce3
Chilka Castro <chilcjcastro@yahoo.ca>
2022-04-05 09:20:30 -0400

    Did modifications on the editProfile

---
+++ BookStore/app/views/Admin/editProfile.php
@@ -0,0 +1,6 @@
+<?php require APPROOT . '/views/includes/adminheader.php';  ?>  
+<div class="container" style="min-height: 100vh;">
+        <h1 style="margin-bottom: 25px;margin-top: 50px;text-align: center;">Profile</h1>
+    </div>
+
+<?php require APPROOT . '/views/includes/adminfooter.php';  ?>
