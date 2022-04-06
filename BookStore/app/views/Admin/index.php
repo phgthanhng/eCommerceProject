fe3be326be707aff179a595fd2fdfaab9b015efe
phgthanhng <1934520@edu.vaniercollege.qc.ca>
2022-04-05 21:07:52 -0400

    fix buttons in admin view

--- BookStore/app/views/Admin/index.php
+++ BookStore/app/views/Admin/index.php
@@ -5,18 +5,25 @@
         background: url('<?php echo URLROOT; ?>/img/admin_dashboard.jpg');
         margin: 0px;
         padding: 0px;">
-    <button class="btn btn-primary" type="button" style="background: #3e6259; margin: 10px; width: 192px">
-        <a href="<?php echo URLROOT; ?>/Admin/manageProducts" style="margin: auto"></a>Manage Products</button><button class="btn btn-primary"
-        type="button" style="background: #3e6259; margin: 10px; width: 192px">
-        <a href="<?php echo URLROOT; ?>/Admin/addProduct" style="margin: auto"></a>Add a product</button><button class="btn btn-primary"
-        type="button" style="margin: 10px; width: 192px; background: #3e6259">
-        <a href="<?php echo URLROOT;?>/Admin/managerOrders" style="margin: auto"></a>Manage orders</button><button class="btn btn-primary"
-        type="button" style="margin: 10px; width: 192px; background: #3e6259">
-        <a href="<?php echo URLROOT; ?>/Admin/returns" style="margin: auto"></a>return orders</button><button class="btn btn-primary"
-        type="button" style="margin: 10px; width: 192px; background: #3e6259">
-        <a href="<?php echo URLROOT; ?>/Admin/ViewReviews" style="margin: auto"></a>View reviews</button><button class="btn btn-primary"
-        type="button" style="margin: 10px; width: 192px; background: #3e6259">
-        <a href="<?php echo URLROOT; ?>/Admin/editContact" style="margin: auto"></a>Edit contact us
-    </button>
+
+    <div class="text-center d-lg-flex flex-column justify-content-center align-content-center align-items-lg-center" style="min-height: 100vh;background: url(&quot;../assets/img/admin_dashboard.jpg&quot;);margin: 0px;padding: 0px;">
+        <a href="<?php echo URLROOT; ?>/Admin/manageProducts" style="margin: auto;margin-top: 30pxpx;margin-bottom: 30px;">
+        <button class="btn btn-primary" type="button" style="background: #3e6259;margin: 0px;width: 192px;">Manage Products</button>
+        </a>
+        <a href="<?php echo URLROOT; ?>/Admin/addProduct" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
+        <button class="btn btn-primary" type="button" style="background: #3e6259;margin: 0px;width: 192px;">Add a product</button>
+        </a>
+        <a href="<?php echo URLROOT; ?>/Admin/manageOrder" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
+        <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">Manage orders</button>
+        </a>
+        <a href="<?php echo URLROOT; ?>/Admin/returns" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
+        <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">return orders</button>
+        </a>
+        <a href="<?php echo URLROOT; ?>/Admin/reviews" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
+        <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">View reviews</button>
+        </a>
+        <a href="<?php echo URLROOT; ?>/Admin/editContactUs" style="margin: auto;margin-top: 0px;"><button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">Edit contact us</button>
+        </a>
+    </div>
 </div>
 <?php require APPROOT . '/views/includes/adminfooter.php';  ?>
