9951af60db355e95021a91f99a2d2d0d4a85259c
Chilka Castro <chilcjcastro@yahoo.ca>
2022-04-06 11:02:16 -0400

    simplified the code of the access block

--- BookStore/app/controllers/Admin.php
+++ BookStore/app/controllers/Admin.php
@@ -4,21 +4,17 @@ class Admin extends Controller
     public function __construct()
     {
         $this->bookModel = $this->model('bookModel');
-
+        if (!isLoggedIn()) {
+            header('Location: /eCommerceProject/BookStore/Login');
+        }
     }
 
     public function index()
     {
-        if (isLoggedIn()) {
-            $this->view('Admin/index');
-        }
-        else {
-            $this->view('Home/index');
-        }
+        $this->view('Admin/index');  
     }
 
     public function imageUpload(){
-        if (isLoggedIn()) {
         //default value for the picture
         $filename=false;
         
@@ -44,41 +40,37 @@ class Admin extends Controller
             move_uploaded_file($file['tmp_name'], "$folder/$filename");
         }
         return $filename;
-        }
-        else {
-            $this->view('Home/index');
-        }
+        
     }
 
     public function addBook(){
-        if (isLoggedIn()) {
-            if(!isset($_POST['addBook'])){
-                $this->view('Admin/addBook');
-            }
-            else{
-                $filename= $this->imageUpload();
-                $data=[
-                    'bookname' => trim($_POST['title']),
-                    'isbn' => trim($_POST['isbn']),
-                    'author' => trim($_POST['author']),
-                    'publisher' => trim($_POST['publisher']),
-                    'retailprice' => trim($_POST['price']),
-                    'availablequantity' => trim($_POST['quantity']),
-                    'image' => $filename,
-                    'description' => trim($_POST['desc']),
-                    'category' => trim($_POST['category'])
-                ];
-               
-                if($this->bookModel->addBook($data)){
-                    echo 'Book Addded!';
-                    echo '<meta http-equiv="Refresh" content="2; url=/eCommerceProject/BookStore/Admin/index">';
-                }
+        if(!isset($_POST['addBook'])){
+            $this->view('Admin/addBook');
+        }
+        else{
+            $filename= $this->imageUpload();
+            $data=[
+                'bookname' => trim($_POST['title']),
+                'isbn' => trim($_POST['isbn']),
+                'author' => trim($_POST['author']),
+                'publisher' => trim($_POST['publisher']),
+                'retailprice' => trim($_POST['price']),
+                'availablequantity' => trim($_POST['quantity']),
+                'image' => $filename,
+                'description' => trim($_POST['desc']),
+                'category' => trim($_POST['category'])
+            ];
+            
+            if($this->bookModel->addBook($data)){
+                echo 'Book Addded!';
+                echo '<meta http-equiv="Refresh" content="2; url=/eCommerceProject/BookStore/Admin/index">';
             }
         }
+    
     }
 
     public function manageProducts() {
-        if (isLoggedIn()){
+        
             // $this->view('Admin/manageProducts');
             $books = $this->bookModel->getAllBooks();
             
@@ -89,10 +81,7 @@ class Admin extends Controller
 
                 $this->view('Admin/manageProducts', $data);
             }
-        }
-        else {
-            $this->view('Home/index');
-        }
+       
     }
 
     public function details($bookID) {
@@ -105,27 +94,19 @@ class Admin extends Controller
     // }
 
     public function delete($bookID) {
-        if (isLoggedIn()) {
-            $data = [
-                'bookID' => $bookID
-            ];
-            if ($this->bookModel->delete($data)) {
-                echo 'Please wait we are deleting the book for you!';
-                echo '<meta http-equiv="Refresh" content=".2; url='.URLROOT.'/Admin/manageProducts">';
-            }
+        $data = [
+            'bookID' => $bookID
+        ];
+        if ($this->bookModel->delete($data)) {
+            echo 'Please wait we are deleting the book for you!';
+            echo '<meta http-equiv="Refresh" content=".2; url='.URLROOT.'/Admin/manageProducts">';
         }
-        else {
-            $this->view('Home/index');
-        }
+       
     }
 
     public function editProfile() {
-        if (isLoggedIn()) {
         // just take sessionid as the data 
         $this->view('Admin/editProfile');
-        }
-        else {
-            $this->view('Home/index');
-        }
+        
     }
 }
