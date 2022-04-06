6635404deb8851a7184091f3c5b8b8e508cf99fc
phgthanhng <1934520@edu.vaniercollege.qc.ca>
2022-04-06 01:41:00 -0400

    footer

--- BookStore/app/views/includes/footer.php
+++ BookStore/app/views/includes/footer.php
@@ -1,17 +1,11 @@
- <footer class="footer-basic" style="background: #bdc696; position:fixed; left:0; bottom:0; width:100%">
-    <div class="social">
+ <footer class="footer-basic" style="background: #bdc696; position:fixed; left:0; bottom:0; width:100%;">
+    <div class="social"">
       <a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://www.snapchat.com/"><i
           class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/?lang=en"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com/"><i
           class="icon ion-social-facebook"></i></a>
     </div>
-    <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Home</a></li>
-      <li class="list-inline-item"><a href="#">Services</a></li>
-      <li class="list-inline-item"><a href="#">About</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-    </ul>
-    <p class="copyright">AllBookedUp © 2022</p>
+   
+    <p class="copyright" style="color: #ffffff;">AllBookedUp © 2022</p>
   </footer>
   <script src="<?php echo URLROOT; ?>/bootstrap/js/bootstrap.min.js"></script>
   <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
