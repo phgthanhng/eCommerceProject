93dd9cc8e61f11cf49aee8ee4d8cb1ddd95e7be6
phgthanhng <1934520@edu.vaniercollege.qc.ca>
2022-04-06 14:10:28 -0400

    form validation

--- BookStore/app/views/Admin/addBook.php
+++ BookStore/app/views/Admin/addBook.php
@@ -5,38 +5,44 @@
     <h1 style="margin-top: 34px;">Add a book</h1>
     <form method="post" action="" enctype="multipart/form-data"
         style="width: 600px;height: 65 0px;margin: 22px;margin-top: 7px;background: rgba(112,126,101,0.53);padding-top: 17px;padding-right: -6px;">
-        <div><input class="form-control float-end" type="text"
+        <div><input class="form-control float-end" type="text" required="" minlength="10"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 9px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="title" /><label class="form-label float-none"
                 style="font-size: 22px;width: 188px;height: 26.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 7px;">Title</label>
         </div>
-        <div><label class="form-label float-none"
-                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 10px;">ISBN</label><input
+        <div><label class="form-label float-none" 
+                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 10px;">ISBN</label>
+                <input required="" minlength="10"
                 class="form-control float-end" type="text"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 9px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="isbn" /></div>
-        <div><label class="form-label float-none"
-                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 6px;">Author</label><input
+        <div><label class="form-label float-none" 
+                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 6px;">Author</label>
+                <input required="" minlength="10"
                 class="form-control float-end" type="text"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 9px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="author" /></div>
-        <div><label class="form-label float-none"
-                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 6px;">Category</label><input
+        <div><label class="form-label float-none" 
+                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 6px;">Category</label>
+                <input required="" minlength="10"
                 class="form-control float-end" type="text"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 9px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="category" /></div>
-        <div><label class="form-label float-none"
-                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 5px;margin-bottom: 17px;">Publisher</label><input
+        <div><label class="form-label float-none" 
+                style="font-size: 22px;width: 188px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 5px;margin-bottom: 17px;">Publisher</label>
+                <input required="" minlength="10"
                 class="form-control float-end" type="text"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 9px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="publisher" />
             <div><label class="form-label float-start"
                     style="font-size: 22px;width: 227px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: -1px;">Retail
-                    Price</label><input class="form-control float-end" type="text"
+                    Price</label>
+                    <input class="form-control float-end" type="number" step="0.01" required=""
                     style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: -34px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;margin-bottom: 13px;"
                     name="price" /></div>
         </div>
-        <div><input class="form-control float-end" type="number"
+        <div>
+                <input class="form-control float-end" type="number" required="" min="1"
                 style="width: 364px;padding: 0px 0px 0px 0px;padding-top: 2px;margin-right: 10px;margin-top: 10px;font-size: 12px;height: 29px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"
                 name="quantity" /><label class="form-label float-start"
                 style="font-size: 22px;width: 227px;height: 28.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: -34px;margin-bottom: 15px;">Available
@@ -50,7 +56,7 @@
         </div>
         <div><label class="form-label float-start"
                 style="font-size: 22px;width: 65px;height: 14.5px;color: rgb(0,0,0);font-weight: bold;text-align: center;margin-top: 14px;margin-left: 66px;margin-right: 8px;">Image</label>
-        <textarea name="desc"
+        <textarea name="desc" required="" minlength="10"
                 class="form-control float-end"
                 style="overflow-y: scroll;width: 364px;height: 159px;margin-top: 20px;margin-right: 10px;padding: 0px 0px 0px 0px;padding-top: 2px;padding-right: 2px;padding-bottom: 2px;padding-left: 2px;"></textarea>
         </div><button class="btn btn-primary float-end" type="submit" name="addBook"
