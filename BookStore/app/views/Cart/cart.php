3ba29fdc15b398e866398ab7cd5daf6c52ecb22e
phgthanhng <1934520@edu.vaniercollege.qc.ca>
2022-04-13 22:33:50 -0400

    merging

--- BookStore/app/views/Cart/cart.php
+++
@@ -1,8 +0,0 @@
-<?php require APPROOT . '/views/includes/header.php';  ?>
-
-<!-- <body style="min-height: 100vh;"> -->
-<div class="container">
-    <div data-reflow-type="shopping-cart" data-reflow-success-url="/success.html" data-reflow-cancel-url="/cancel.html">
-    </div>
-</div>
-<?php require APPROOT . '/views/includes/footer.php';  ?>
