cad0bf1a2e0237f105046165a8240bc9d6d3b592
phgthanhng <1934520@edu.vaniercollege.qc.ca>
2022-04-20 09:57:02 -0400

    merging

--- BookStore/app/views/Admin/index.php
+++ BookStore/app/views/Admin/index.php
@@ -9,7 +9,6 @@
     <div class="text-center d-lg-flex flex-column justify-content-center align-content-center align-items-lg-center"
         style="min-height: 100vh;margin: 0px;padding: 0px;">
         <a href="<?php echo URLROOT; ?>/Admin/manageProducts" style="margin-top: 10px;margin-bottom: 30px;">
-<<<<<<< Updated upstream
         <button class="btn btn-primary" type="button" style="background: #ffffff;margin: 0px;width: 192px;"><p style="margin:auto; font-weight:900; color:#3e6259;">Manage Products</p></button>
         </a>
         <a href="<?php echo URLROOT; ?>/Admin/addBook" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
@@ -26,30 +25,6 @@
         </a>
         <a href="<?php echo URLROOT; ?>/Admin/displayUsers" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
         <button class="btn btn-primary" type="button" style="background: #ffffff;margin: 0px;width: 192px; font-weight:bolder; color:#3e6259;"><p style="margin:auto; font-weight:900; color:#3e6259;">Display all Customers</p></button>
-=======
-            <button class="btn btn-primary" type="button" style="background: #3e6259;margin: 0px;width: 192px;">Manage
-                Products</button>
-        </a>
-        <a href="<?php echo URLROOT; ?>/Admin/addBook" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
-            <button class="btn btn-primary" type="button" style="background: #3e6259;margin: 0px;width: 192px;">Add a
-                product</button>
-        </a>
-        <a href="<?php echo URLROOT; ?>/Admin/manageOrder" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
-            <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">Manage
-                orders</button>
-        </a>
-        <a href="<?php echo URLROOT; ?>/Admin/returns" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
-            <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">return
-                orders</button>
-        </a>
-        <a href="<?php echo URLROOT; ?>/Admin/reviews" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
-            <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">View
-                reviews</button>
-        </a>
-        <a href="<?php echo URLROOT; ?>/Admin/displayUsers" style="margin: auto;margin-top: 0px;margin-bottom: 30px;">
-            <button class="btn btn-primary" type="button" style="margin: 0px;width: 192px;background: #3e6259;">Display
-                all Customers</button>
->>>>>>> Stashed changes
         </a>
 
     </div>
