b4a27203c7e8a2da2232d69b5cac9de72a3e1a3f
Jiahui Xia <xiajiahui2002@163.com>
2022-04-28 01:42:56 -0400

    fix categories views

--- BookStore/app/views/Categories/fiction.php
+++ BookStore/app/views/Categories/fiction.php
@@ -2,73 +2,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<style>
-/* html {
-        position: relative;
-        min-height: 100%;
-    } */
-
-
-
-
-.parent {
-
-    max-width: 80%;
-    overflow: hidden;
-    zoom: 1;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-}
-
-.item {
-    /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); */
-    width: 230px;
-    float: left;
-    height: auto;
-    text-align: center;
-    font-family: arial;
-}
-
-.bookname {
-    color: black;
-    font-size: 18px;
-    /* line-height: 10px; */
-
-}
-
-.author {
-    color: black;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 10px;
-
-}
-
-.price {
-    color: black;
-    font-size: 16px;
-    line-height: 10px;
-}
-
-.item button {
-    border: none;
-    outline: 0;
-    padding: 8px;
-    color: white;
-    background-color: #8BAD57;
-    text-align: center;
-    cursor: pointer;
-    width: 100%;
-    font-size: 18px;
-}
-
-.card button:hover {
-    opacity: 0.7;
-}
-</style>
-
 <body>
     <!-- <div class="container"> -->
     <h1 style="margin-bottom: 25px;margin-top: 30px;text-align: center;">ALL BOOKS</h1>
@@ -78,26 +11,35 @@
             foreach ($data["fiction_books"] as $book) {
                 $id = $book->bookID;
                 $name = substr($book->bookname, 0, 20) . "...";
+                $author = $book->author;
+                if(strpos($author, ",")){
+                    $author = substr($book->author, 0, 19) . "...";
+                }else{
+                    $author = $book->author;
+                }
+                
                 echo '<div class = "item">';
-                echo '<a href="' . URLROOT . '/Book/bookdetail/'.$id.'">
-                        <img src="' . URLROOT . '/public/img/' . $book->image . '" style="width:210px; height:300px;"
+                echo '<a href="'.URLROOT.'/Book/bookdetail/'.$book->bookID.'">
+                    <img src="' . URLROOT . '/public/img/' . $book->image . '" style="width:210px; height:300px;"
                     </a>';
-                echo '<p><a href="' . URLROOT . '/Book/bookdetail/' . $id . '" style="text-decoration:none;"> ' . $name . '</a></p>';
-                echo '<p class="author"> by ' . $book->author . '</p>';
+                echo '<p><a href="' . URLROOT . '/Book/bookdetail/' . $id . '" style="text-decoration: none;"> ' . $name . '</a></p>';
+                echo '<p class="author"> by ' .$author . '</p>';
                 echo '<p class="price"> $' . $book->retailprice . "</p>";
+               
                 // if not logged in 
                 if (!isLoggedIn()) {
-                    echo '<p><button>Add to Cart</button></p>';
+                    echo '<p>
+                        <a href="' .URLROOT. '/Login/index/"><button>Add to Cart</button></a>
+                        </p>';
                 }
                 // if is logged in
                 else {
                     // if users -> show the Add to Cart
                     if (!isAdmin()) {
-                           echo '<p>
+                        echo '<p>
                             <a href="' .URLROOT. '/Book/bookdetail/'.$id.'"><button>Add to Cart</button></a>
                             </p>';
                     }
-                    
                 }
                 echo '</div>';
             }
